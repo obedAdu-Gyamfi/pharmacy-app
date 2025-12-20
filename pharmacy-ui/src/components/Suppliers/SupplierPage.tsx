5b2c3299608ce91e19f8cf7996abd990bed86b1d
Obed Adu-Gyamfi <adugyamfiobed.tpp3@gmail.com>
2025-12-20 19:51:47 +0000

    refactor(frontend): apply multiple updates across different components

--- pharmacy-ui/src/components/Suppliers/SupplierPage.tsx
+++ pharmacy-ui/src/components/Suppliers/SupplierPage.tsx
@@ -1,18 +1,118 @@
-import { Outlet } from 'react-router-dom';
-import { SalesTopBar } from '../Sales/SalesTopBar';
-import { SearchBar } from '../Sidebar/SearchBar';
+import { Outlet } from "react-router-dom";
+import { SalesTopBar } from "../Sales/SalesTopBar";
+import { SearchBar } from "../Sidebar/SearchBar";
+import { useState } from "react";
 
 export const SupplierPage = () => {
+  const [supplier_id, setSupplierID] = useState("");
+  const [name, setName] = useState("");
+  const [contact_person, setContactPerson] = useState("");
+  const [email, setEmail] = useState("");
+  const [phone, setPhone] = useState("");
+
+  const handleSupplierSelect = (supplier: {
+    id?: number | string;
+    name?: string;
+    contact_person?: string;
+    email?: string;
+    phone?: string;
+  }) => {
+    setSupplierID(supplier.id?.toString() || "");
+    setName(supplier.name || "");
+    setContactPerson(supplier.contact_person || "");
+    setEmail(supplier.email || "");
+    setPhone(supplier.phone || "");
+  };
   return (
     <div className="flex flex-col min-h-screen bg-gray-100">
       <SalesTopBar />
       <SearchBar
-              endpoint="/search-supplier/"
-              onSelect={(supplier) => console.log(supplier)}
-            />
-      <main className="flex-1 p-4">
+        endpoint="/search-supplier/"
+        onSelect={handleSupplierSelect}
+      />
+      <main className="flex p-4">
         <Outlet />
       </main>
+      <div className="rounded border border-stone-300 p-4">
+        <form className="space-y-4">
+          <div>
+            <label className="block mb-1 text-gray-600" htmlFor="supplier_id">
+              Supplier ID
+            </label>
+            <input
+              type="text"
+              id="supplier_id"
+              name="supplier_id"
+              placeholder="supplier id"
+              value={supplier_id}
+              onChange={(e) => setSupplierID(e.target.value)}
+              className="w-full px-4 py-2 border border-gray-300 rounded-full focus:outline-none focus:ring-2 focus:ring-blue-500"
+            />
+          </div>
+
+          <div>
+            <label className="block mb-1 text-gray-600" htmlFor="name">
+              Name
+            </label>
+            <input
+              type="name"
+              id="name"
+              name="name"
+              placeholder="name"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+              className="w-full px-4 py-2 border border-gray-300 rounded-full focus:outline-none focus:ring-2 focus:ring-blue-500"
+            />
+          </div>
+
+          <div>
+            <label
+              className="block mb-1 text-gray-600"
+              htmlFor="contact_person"
+            >
+              Contact Person
+            </label>
+            <input
+              type="text"
+              id="contact_person"
+              name="contact_person"
+              placeholder="contact person"
+              value={contact_person}
+              onChange={(e) => setContactPerson(e.target.value)}
+              className="w-full px-4 py-2 border border-gray-300 rounded-full focus:outline-none focus:ring-2 focus:ring-blue-500"
+            />
+          </div>
+
+          <div>
+            <label className="block mb-1 text-gray-600" htmlFor="email">
+              Email
+            </label>
+            <input
+              id="email"
+              name="email"
+              value={email}
+              placeholder="email"
+              onChange={(e) => setEmail(e.target.value)}
+              className="w-full px-4 py-2 border border-gray-300 rounded-full bg-white focus:outline-none focus:ring-2 focus:ring-blue-500"
+            />
+          </div>
+
+          <div>
+            <label className="block mb-1 text-gray-600" htmlFor="phone">
+              Phone
+            </label>
+            <input
+              type="text"
+              id="phone"
+              name="phone"
+              placeholder="phone"
+              value={phone}
+              onChange={(e) => setPhone(e.target.value)}
+              className="w-full px-4 py-2 border border-gray-300 rounded-full focus:outline-none focus:ring-2 focus:ring-blue-500"
+            />
+          </div>
+        </form>
+      </div>
     </div>
   );
-}
+};
--- pharmacy-ui/src/components/Users/UsersPage.tsx
+++ pharmacy-ui/src/components/Users/UsersPage.tsx
@@ -1,21 +1,111 @@
 import { Outlet } from "react-router-dom";
 import { SalesTopBar } from "../Sales/SalesTopBar";
 import { SearchBar } from "../Sidebar/SearchBar";
+import { useState } from "react";
 
 export const Users = () => {
-  return (
-    <div className="flex flex-col min-h-screen bg-gray-100">
-      <SalesTopBar />
-      <SearchBar
-        endpoint="/search-user/"
-        onSelect={(user) => console.log("Selected user", user)}
-      />
-      <main>
-        {/* <Link to="create-user" className="text-blue-600 hover:underline">
+
+  const [user_id, setUserID] = useState("");
+  const [username, setUsername] = useState("");
+  const [fullname, setFullname] = useState("");
+  const [email, setEmail] = useState("");
+  const [role, setRole] = useState("");
+
+const handleUserSelect = (user: { id?: number | string; username?: string; fullname?: string; email?: string; role?: string }) =>{
+  setUserID(user.id?.toString() || "");
+  setUsername(user.username || "");
+  setFullname(user.fullname || "");
+  setEmail(user.email || "");
+  setRole(user.role || "")
+}
+
+return (
+  <div className="flex flex-col min-h-screen bg-gray-100">
+    <SalesTopBar />
+    <SearchBar endpoint="/search-user/" onSelect={handleUserSelect} />
+    <main className="flex p-4">
+      {/* <Link to="create-user" className="text-blue-600 hover:underline">
           <FiArrowLeft /> Back{" "}
         </Link> */}
-        <Outlet />
-      </main>
+      <Outlet />
+    </main>
+    <div className="rounded border border-stone-300 p-4">
+      <form className="space-y-4">
+        <div>
+          <label className="block mb-1 text-gray-600" htmlFor="user_id">
+            User ID
+          </label>
+          <input
+            type="text"
+            id="user_id"
+            name="user_id"
+            placeholder="Enter your user_id"
+            value={user_id}
+            onChange={(e) => setUserID(e.target.value)}
+            className="w-full px-4 py-2 border border-gray-300 rounded-full focus:outline-none focus:ring-2 focus:ring-blue-500"
+          />
+        </div>
+
+        <div>
+          <label className="block mb-1 text-gray-600" htmlFor="username">
+            Username
+          </label>
+          <input
+            type="username"
+            id="username"
+            name="username"
+            placeholder="Enter your username"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+            className="w-full px-4 py-2 border border-gray-300 rounded-full focus:outline-none focus:ring-2 focus:ring-blue-500"
+          />
+        </div>
+
+        <div>
+          <label className="block mb-1 text-gray-600" htmlFor="fullname">
+            Full Name
+          </label>
+          <input
+            type="text"
+            id="fullname"
+            name="fullname"
+            placeholder="Enter your full name"
+            value={fullname}
+            onChange={(e) => setFullname(e.target.value)}
+            className="w-full px-4 py-2 border border-gray-300 rounded-full focus:outline-none focus:ring-2 focus:ring-blue-500"
+          />
+        </div>
+
+        <div>
+          <label className="block mb-1 text-gray-600" htmlFor="email">
+            Email
+          </label>
+          <input
+            id="email"
+            name="email"
+            value={email}
+            placeholder="email"
+            onChange={(e) => setEmail(e.target.value)}
+            className="w-full px-4 py-2 border border-gray-300 rounded-full bg-white focus:outline-none focus:ring-2 focus:ring-blue-500"
+          />
+        </div>
+
+        <div>
+          <label className="block mb-1 text-gray-600" htmlFor="role">
+            Role
+          </label>
+          <input
+            type="text"
+            id="role"
+            name="role"
+            placeholder="role"
+            value={role}
+            onChange={(e) => setRole(e.target.value)}
+            className="w-full px-4 py-2 border border-gray-300 rounded-full focus:outline-none focus:ring-2 focus:ring-blue-500"
+          />
+        </div>
+      </form>
     </div>
-  );
+  </div>
+);
 };
